b4ac91c0bbc819b48dbae66da9442c4d160052e5
HypeR <gperalta672@gmail.com>
2026-02-26 02:02:16 -0500

    feat: add Navbar and Footer components and update index.html.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,20 +2,20 @@ import styles from './Navbar.module.css';
 
 export default function Navbar() {
   return (
-    <nav className={styles.nav}>
+    <header className={styles.nav}>
       <div className={`container ${styles.inner}`}>
         <a href="#" className={styles.logo}>ITZHYPER.PORTFOLIO</a>
 
-        <div className={styles.links}>
+        <nav className={styles.links}>
           <a href="#work" className={styles.navLink}>WORK</a>
           <a href="#about" className={styles.navLink}>ABOUT</a>
           <a href="#contact" className={styles.ctaBtn}>GET IN TOUCH</a>
-        </div>
+        </nav>
 
         <button className={styles.menuBtn} type="button" aria-label="Open menu">
           <span className="material-icons">menu</span>
         </button>
       </div>
-    </nav>
+    </header>
   );
 }
