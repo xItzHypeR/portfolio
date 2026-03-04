81b893f7af3aa8b57c1c7280eb4cd35f1eb29957
HypeR <gperalta672@gmail.com>
2026-03-04 00:09:23 -0500

    feat: Add Projects section with individual project cards and introduce initial Navbar components.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,24 @@
 import styles from './Navbar.module.css';
+import { useState } from 'react';
+import Checkbox from './Checkbox';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className={styles.nav}>
       <div className={`container ${styles.inner}`}>
         <a href="#hero" className={styles.logo}>ITZHYPER.PORTFOLIO</a>
 
-        <nav className={styles.links}>
-          <a href="#about" className={styles.navLink}>ABOUT</a>
-          <a href="#contact" className={styles.ctaBtn}>GET IN TOUCH</a>
-        </nav>
+        <nav className={styles.links + ' ' + (isOpen ? styles.open : '')}>
+          <a href="#about" className={styles.navLink} onClick={() => setIsOpen(false)} >ABOUT</a>
+          <a download href="/CV_Gustavo_Peralta.pdf" className={styles.outlineBtn} onClick={() => setIsOpen(false)}>DOWNLOAD RESUME</a>
+          <a href="#contact" className={styles.ctaBtn} onClick={() => setIsOpen(false)}>GET IN TOUCH</a>
 
-        <button className={styles.menuBtn} type="button" aria-label="Open menu">
-          <span className="material-icons">menu</span>
-        </button>
+        </nav>
+        <div className={styles.menuBtn}>
+          <Checkbox handleMenuToggle={() => setIsOpen(!isOpen)} isOpen={isOpen} />
+        </div>
       </div>
     </header>
   );
